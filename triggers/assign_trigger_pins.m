a0ba8c4cc6783f61cff1d4a545fc7a6c011aa0f3
younahjeon <hiyuna1993@gmail.com>
2022-10-24 20:13:02 -0400

    restage files

---
+++ triggers/assign_trigger_pins.m
@@ -0,0 +1,43 @@
+function assign_trigger_pins(trigger_arduino)
+% three types of triggers: 
+% trial: goes on at beginning of trial (inlcudes pre-stim initiation period) 
+% stimulus: goes on at presentation of stimulus, including for each RSVP
+% session: goes on at beginning of behavior session and off after all
+% trials or exist
+
+ahand = trigger_arduino.ahand;
+trial_trig_pin = trigger_arduino.trial_pin;
+stim_trig_pin = trigger_arduino.stim_pin;
+session_trig_pin = trigger_arduino.session_pin;
+
+trigger_flag = true;
+IOPort('Flush', ahand);
+Alphabet = 'abcdefghijklmnopqrstuvwxyz';
+trial_pin_letter = Alphabet(trial_trig_pin + 1);
+stim_trig_letter = Alphabet(stim_trig_pin + 1);
+session_trig_letter = Alphabet(session_trig_pin + 1);
+
+assign_trialpin_cmd = ['0' trial_pin_letter '1']';
+assign_stimpin_cmd = ['0' stim_trig_letter '1']';
+assign_sesspin_cmd = ['0' session_trig_letter '1']';
+
+IOPort('Write', ahand, assign_sesspin_cmd, 1);
+WaitSecs(0.05);
+IOPort('Write', ahand, assign_sesspin_cmd, 1);
+WaitSecs(0.05); 
+fprintf('Assigned pin %d to session trigger\n', session_trig_pin); 
+
+IOPort('Write', ahand, assign_trialpin_cmd, 1);
+WaitSecs(0.05);
+IOPort('Write', ahand, assign_trialpin_cmd, 1);
+WaitSecs(0.05); 
+fprintf('Assigned pin %d to trial trigger\n', trial_trig_pin); 
+
+IOPort('Write', ahand, assign_stimpin_cmd, 1);
+WaitSecs(0.05);
+IOPort('Write', ahand, assign_stimpin_cmd, 1);
+WaitSecs(0.05); 
+fprintf('Assigned pin %d to stimulus trigger\n', stim_trig_pin); 
+
+
+
---
+++ triggers/gen_trig_commands.m
@@ -0,0 +1,20 @@
+function [sess_trig_cmd, trial_trig_cmd, stim_trig_cmd] = gen_trig_commands(trigger_arduino)
+
+trial_trig_pin = trigger_arduino.trial_pin;
+stim_trig_pin = trigger_arduino.stim_pin;
+session_trig_pin = trigger_arduino.session_pin;
+
+Alphabet = 'abcdefghijklmnopqrstuvwxyz';
+trial_pin_letter = Alphabet(trial_trig_pin + 1);
+stim_trig_letter = Alphabet(stim_trig_pin + 1);
+session_trig_letter = Alphabet(session_trig_pin + 1);
+
+sess_trig_cmd.on = ['2' session_trig_letter '1'];
+sess_trig_cmd.off = ['2' session_trig_letter '0'];
+
+trial_trig_cmd.on = ['2' trial_pin_letter '1'];
+trial_trig_cmd.off = ['2' trial_pin_letter '0'];
+
+stim_trig_cmd.on = ['2' stim_trig_letter '1'];
+stim_trig_cmd.off = ['2' stim_trig_letter '0'];
+
